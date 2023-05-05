0e6a4e9270e0882eb949538be617e7cfcf95df43
Replit user <>
2023-05-05 03:58:29 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -97,8 +97,8 @@ int repetidosSubMatriz(int subMatriz, Node *n)
     
     if(n -> sudo[fila][columna] != 0)
     {
-      if (aux[n -> sudo[fila][columna]] == 1) return 0;
-      else aux[n -> sudo[fila][columna]] = 1;
+      if (aux[n -> sudo[i][j]] == 1) return 0;
+      else aux[n -> sudo[i][j]] = 1;
     }
   }
   
